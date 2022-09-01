fd9c3090380eb916cb69efe75940efd9f7b15c8c
NihatArtan <2005nihat@gmail.com>
2022-09-01 10:31:14 +0200

    Geist

--- Geist/Program.cs
+++ Geist/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("  -  ");
-Console.WriteLine(" .  . ");
-
+Console.WriteLine(" .-. ");
+Console.WriteLine("(o o)  ");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
